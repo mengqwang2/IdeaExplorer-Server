49a1a6cfe0ad2c84ef417410c2b4ca80c6363b72
mengqwang <mengqwang2@gmail.com>
2015-08-07 14:07:07 +0800

    files remove

---
+++ IdeaExplorer/LDA/jsonValidator.py
@@ -0,0 +1,11 @@
+import io, json
+
+class JsonValidator():
+	def __init__(self,fp):
+		self.__fp=fp
+		with open("../data/ideas.txt") as json_file:
+			self.__data=json.load(json_file,strict=False)
+
+	def doUnicode(self):
+		with io.open("../data/ideas_utf8.txt", 'w', encoding='utf-8') as f:
+			f.write(unicode(json.dumps(data, ensure_ascii=False)))
