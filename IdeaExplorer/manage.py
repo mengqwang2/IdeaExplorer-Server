59e105f5a2cafbd3851a6470e212baac8e0018b7
WildVegeJuice <jiahushen2@gmail.com>
2015-08-10 15:21:08 +0900

    Update manage.py

--- IdeaExplorer/manage.py
+++ IdeaExplorer/manage.py
@@ -1,7 +1,8 @@
 # Set the path
 import os, sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
+#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))+'/IdeaExplorer/DBUpdate')
+#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))+'/IdeaExplorer/lib')
 from flask.ext.script import Manager, Server
 from IdeaExplorer import app
 
