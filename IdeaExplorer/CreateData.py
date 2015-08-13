7e61933d355faff82c0a502532dc2f2425ed0f82
WildVegeJuice <jiahushen2@gmail.com>
2015-08-13 02:24:50 -0400

    Update CreateData.py

--- IdeaExplorer/CreateData.py
+++ IdeaExplorer/CreateData.py
@@ -3,7 +3,8 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import onlineIdea
 import tagGen
 
-# Generate lambda, gamma and Tag set in the data file.
+# Generate lambda, gamma and Tag set in the data file
+# For the parameter, please refer to tagGen and onlineIdea
 def main():
     tagGen.TagGen(docNum = 5000).generateAllTags()
     onlineIdea.IdeaLDA(batchsize = 50,d = 5000 ,k = 100 ,tau = 1024 ,kappa = 0.7).runLDA()
